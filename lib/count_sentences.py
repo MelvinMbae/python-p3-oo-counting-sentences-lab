66a792427638cb45dbaa7b347774a628662c0709
MelvinMbae <melvinmuthomi95@gmail.com>
2023-10-15 01:57:10 +0300

    comments added

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 class MyString:
-  def __init__(self, value=0):
+  def __init__(self, value=""):
     self._value = value
   
   def get_value(self):
@@ -28,10 +28,15 @@ class MyString:
     return self._value.endswith("!")
   
   def count_sentences(self):
+    # check if self._value is falsey. if falsey it returns 0
     if not self._value:
       return 0
+    # introduce new characters not existing in original string so as to split
     value2 = self._value.replace("." , "/").replace("!", "/").replace("?", "/").split("/")
     print(value2)
+    
+    # Use list comprehension to append valid sentences i.e. sentences that are not empty
+    # remove white spaces. if sentence.strip checks if the sentence after stripping the white space it is not falsey/empty
     valid_sentences = [sentence for sentence in value2 if sentence.strip()]
     print(valid_sentences)
     return len(valid_sentences) 
